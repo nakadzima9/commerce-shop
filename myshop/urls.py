70cdcbe0b87c7cfb5cae587feb06317a70d91f90
Kadzima <pascal.german@yandex.ru>
2021-07-23 17:01:24 +0600

    updated myshop/urls

--- myshop/urls.py
+++ myshop/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('cart/', include('cart.urls', namespace='cart')),
     path('orders/', include('orders.urls', namespace='orders')),
     path('', include('shop.urls', namespace='shop')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
+]
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
